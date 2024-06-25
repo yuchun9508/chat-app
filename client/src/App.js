507c2dbb5e717bf876ebab286f40a19017be554b
Chun <yuchun9508@gmail.com>
2024-06-25 15:06:36 +0800

    Add socket io connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import socketIO from 'socket.io-client';
+
+const socket = socketIO.connect('http://localhost:4000');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world!</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,19 @@ const PORT = 4000;
 const http = require('http').Server(app);
 const cors = require('cors');
 
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+socketIO.on('connection', (socket) => {
+  console.log(`${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
 app.use(cors());
 
 app.get('/api', (req, res) => {
