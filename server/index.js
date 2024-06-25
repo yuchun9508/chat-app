5d49b5a6e492c4b275e18806b695eec77ce4493c
Chun <yuchun9508@gmail.com>
2024-06-25 14:41:39 +0800

    Set up server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
