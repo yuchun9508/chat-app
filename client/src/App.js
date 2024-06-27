f2f236a07183c6e6af7e55137770be60d36815e2
Chun <yuchun9508@gmail.com>
2024-06-27 17:37:15 +0800

    Add `ChatRoom` sidebar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.tsx';
+import ChatRoom from './components/ChatRoom.tsx';
 import socketIO from 'socket.io-client';
 
 const socket = socketIO.connect('http://localhost:4000');
@@ -9,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/chat-room" element={}></Route> */}
+        <Route path="/chat-room" element={<ChatRoom />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/ChatRoom.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const ChatRoom = () => {
+  return (
+    <div className="grid grid-cols-4 h-screen">
+      <div className="col-span-1 bg-slate-900 text-slate-50 pl-14 pr-2 py-10">
+        <div className="flex flex-col h-full">
+          <h3 className="mb-10">In Room (3)</h3>
+          <div className="flex-1 overflow-y-auto">
+            <ul className="max-h-0">
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+              <li>Neque</li>
+              <li>Amklrmv</li>
+              <li>Encmlv</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-3 bg-slate-50 px-14 py-10">Chatbox</div>
+    </div>
+  );
+};
+
+export default ChatRoom;
