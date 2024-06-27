98f4f515880a2ae5cc3e505fc7940d1e2684523b
Chun <yuchun9508@gmail.com>
2024-06-27 23:23:36 +0800

    Update `ChatRoom` layout and add `MessageBubble` component

--- client/src/components/ChatRoom.tsx
+++ client/src/components/ChatRoom.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ArrowForwardIcon from './ArrowForwardIcon.tsx';
+import MessageBubble from './MessageBubble.tsx';
 
 const ChatRoom = () => {
   return (
@@ -48,7 +50,92 @@ const ChatRoom = () => {
           </div>
         </div>
       </div>
-      <div className="col-span-3 bg-slate-50 px-14 py-10">Chatbox</div>
+      <div className="col-span-3 bg-slate-50 px-14 py-10">
+        <div className="flex flex-col h-full divide-y">
+          <div className="flex-1 overflow-y-auto">
+            <div className="max-h-0">
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                isPrimary
+              />
+              <MessageBubble
+                message="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                userName="Neque"
+              />
+            </div>
+          </div>
+          <div className="pt-10">
+            <form>
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  placeholder="Write message"
+                  className="flex-1 outline-none border rounded-md border-gray-200 text-slate-800 placeholder:text-slate-300 py-4 px-3"
+                />
+                <button className="w-16 h-16 bg-gray-200 border rounded-md flex justify-center items-center">
+                  <ArrowForwardIcon />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/components/MessageBubble.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type MessageBubbleProps = {
+  isPrimary?: boolean;
+  userName?: string;
+  message: string;
+};
+
+const MessageBubble = (props: MessageBubbleProps) => {
+  const { isPrimary = false, userName = '', message } = props;
+
+  return (
+    <div className="w-full pb-7">
+      <div className={`${isPrimary && 'ml-auto'} w-2/5`}>
+        <div className={`${isPrimary && 'hidden'} text-sky-500 pl-3`}>
+          {userName}
+        </div>
+        <div
+          className={`${
+            isPrimary ? 'text-slate-50 bg-sky-500' : 'text-slate-900 bg-white'
+          } p-3 rounded-md`}
+        >
+          {message}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageBubble;
