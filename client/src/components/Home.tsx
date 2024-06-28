51c3f25fe7c858f7fb7a8497b87412abccc6e4eb
Chun <yuchun9508@gmail.com>
2024-06-28 17:39:43 +0800

    Update `Home` page responsive layout

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="flex min-h-screen bg-slate-50">
-      <div className="max-w-screen-sm m-auto">
+      <div className="max-w-72 md:max-w-screen-sm m-auto">
         <h1 className="text-slate-800 text-center mb-16">Let's Chat!</h1>
         <form>
           <input
@@ -11,7 +11,7 @@ const Home = () => {
             name="username"
             id="username"
             placeholder="Enter your name"
-            className="w-full outline-none bg-transparent text-2xl text-slate-800 placeholder:text-slate-300 px-3 py-2 border-b border-slate-800 mb-10"
+            className="w-full outline-none bg-transparent text-xl md:text-2xl text-slate-800 placeholder:text-slate-300 px-3 py-2 border-b border-slate-800 mb-10"
           />
           <button className="btn-primary w-full">Enter The Room</button>
         </form>
